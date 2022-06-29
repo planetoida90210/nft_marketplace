41a77ee8b7b58023d10d8a55c6c5083bc84a23ee
planetoida90210 <galeriakoloru90210@gmail.com>
2022-06-29 22:41:53 +0200

    create route and pages structure

---
+++ pages/create-nft.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const CeateNFT = () => <div>create-nft</div>;
+
+export default CeateNFT;
---
+++ pages/my-nfts.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const MyNFTs = () => (
+  <div>MyNFTS</div>
+);
+
+export default MyNFTs;
