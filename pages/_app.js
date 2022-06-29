a695b86799bb8cb265077925581bb8359c53cc81
planetoida90210 <galeriakoloru90210@gmail.com>
2022-06-29 23:45:31 +0200

    implement navbar component, basic styling logo

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import Script from 'next/script';
+import { ThemeProvider } from 'next-themes';
+// external imports
+import { Navbar, Footer } from '../components';
+import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+const MyApp = ({ Component, pageProps }) => (
+  <ThemeProvider attribute="class">
+    <div className="dark:bg-nft-dark bg-white min-h-screen">
+      <Navbar />
+      <Component {...pageProps} />
+      <Footer />
+    </div>
+    <Script src="https://kit.fontawesome.com/99c5f6502c.js" crossorigin="anonymous" />
+  </ThemeProvider>
+);
 
-export default MyApp
+export default MyApp;
