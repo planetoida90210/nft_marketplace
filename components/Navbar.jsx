2c4b5b958fe5b85e14d8706806672aa7b0259740
planetoida90210 <galeriakoloru90210@gmail.com>
2022-06-30 00:01:08 +0200

    implement change mode dark/light button

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -25,6 +25,21 @@ const Navbar = () => {
           </div>
         </Link>
       </div>
+      <div className="flex flex-initial flex-row justify-end">
+        <div className="flex items-center mr-2">
+          <input
+            type="checkbox"
+            className="checkbox"
+            id="checkbox"
+            onChange={() => {}}
+          />
+          <label htmlFor="checkbox" className="flexBetween w-8 h-4 bg-black rounded-2xl p-1 relative label">
+            <i className="fas fa-sun" />
+            <i className="fas fa-moon" />
+            <div className="w-3 h-3 absolute bg-white rounded-full ball" />
+          </label>
+        </div>
+      </div>
     </nav>
   );
 };
