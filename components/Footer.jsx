462f47c72d84441209950e3f763524734a425f50
planetoida90210 <galeriakoloru90210@gmail.com>
2022-07-01 09:40:44 +0200

    display footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,29 @@
-import React from 'react';
+import Image from 'next/image';
+import { useTheme } from 'next-themes';
 
-const Footer = () => (
-  <div>Footer</div>
-);
+// internal imports
+import images from '../assets/assets';
+import { Button } from '.';
+
+const Footer = () => {
+  const { theme } = useTheme();
+
+  return (
+    <footer className="flexCenter flex-col border-t dark:border-nft-black-1 border-nft-gray-1 sm:py-8 py-16">
+      <div className="flexCenter w-full mt-5 border-t dark:border-nft-black-1 border-nft-gray-1 sm:px-4 px-16">
+        <div className="flexBetween flex-row w-full minmd:w-4/5 sm:flex-col mt-7">
+          <p className="font-poppins dark:text-white text-nft-black-1 font-semibold text-base">CryptoKet, Inc. All Rights Reserved.</p>
+          <div className="flex flex-row sm:mt-4">
+            {[images.instagram, images.twitter, images.telegram, images.discord].map((image, index) => (
+              <div className="mx-2 cursor-pointer ">
+                <Image src={image} key={index} alt="socialmedia button" objectFit="contain" width={24} height={24} className={theme === 'light' && 'filter invert'} />
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
