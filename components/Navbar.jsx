520d1affacdf26883b2a9db7eb18e69c768b2005
planetoida90210 <galeriakoloru90210@gmail.com>
2022-07-01 08:32:55 +0200

    implement logic to mobie navigation bar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -95,6 +95,17 @@ const Navbar = () => {
           ) : (
             <Image src={images.menu} objectFit="contain" width={25} height={25} alt="menu" onClick={() => setIsOpen(true)} className={theme === 'light' && 'filter invert'} />
           )}
+
+          {isOpen && (
+            <div className="fixed inset-0 top-65 dark:bg-nft-dark bg-white z-10 nav-h flex justify-between flex-col">
+              <div className="flex-1 p-4">
+                <MenuItems active={active} setActive={setActive} isMobile />
+              </div>
+              <div className="p-4 border-t dark:border-nft-black-1 border-nft-gray-1">
+                <ButtonGroup setActive={setActive} router={router} />
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </nav>
