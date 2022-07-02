1420902afbbe1aa1eee6090774857721bc8343ce
planetoida90210 <galeriakoloru90210@gmail.com>
2022-07-02 23:12:26 +0200

    hot bids card component jsx display in homepage

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
 import { useState, useEffect, useRef } from 'react';
 import Image from 'next/image';
 import { useTheme } from 'next-themes';
-import { Banner, CreatorCard } from '../components';
 
+import { Banner, CreatorCard, NFTCard } from '../components';
 import images from '../assets/assets';
 import { makeId } from '../utils/makeId';
 
@@ -51,6 +51,7 @@ const Home = () => {
           childStyles="md:text-4xl sm:text-2xl xs=text-xl text-left"
           parentStyles="justify-start mb-6 h-72 sm:h-60 p-12 xs:p-4 xs:h-44 rounded-3xl"
         />
+        {/* Best creators */}
         <div>
           <h1 className="font-poppins dark:text-white text-nft-black-1 text-2xl minlg:text-4xl font-semibold ml-4 xs:ml-0">Best Creators</h1>
           <div className="relative flex-1 max-w-full flex mt-3" ref={parentRef}>
@@ -69,6 +70,28 @@ const Home = () => {
               </>
               )}
             </div>
+          </div>
+        </div>
+        {/* Hot Bids */}
+        <div className="mt-10">
+          <div className="flexBetween mx-4 xs:mx-0 minlg:mx-8 sm:flex-col items-start">
+            <h1 className="flex-1 font-poppins dark:text-white text-nft-black-1 text-2xl minlg:text-4xl font-semibold sm:mb-4">Hot Bids</h1>
+            <div>SearchBar</div>
+          </div>
+          <div className="mt-3 w-full flex flex-wrap justify-start md:jusify-center">
+            {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((i) => (
+              <NFTCard
+                key={`nft-${i}`}
+                nft={{
+                  i,
+                  price: (10 - i * 0.534).toFixed(2),
+                  name: `Nifty NFT ${i}`,
+                  seller: `0x${makeId(3)}...${makeId(4)}`,
+                  owner: `0x${makeId(3)}...${makeId(4)}`,
+                  description: 'Coll NFT on Sale',
+                }}
+              />
+            ))}
           </div>
         </div>
       </div>
