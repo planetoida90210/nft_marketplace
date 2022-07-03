d927f4f3e1ce46878de5a099e209d15391e02d89
planetoida90210 <galeriakoloru90210@gmail.com>
2022-07-03 22:16:11 +0200

    set useContext, implement context to hardcode variables in jsx

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -30,7 +30,7 @@ const MenuItems = ({ isMobile, active, setActive }) => {
 };
 
 const ButtonGroup = ({ setActive, router }) => {
-  const hasConnected = false;
+  const hasConnected = true;
 
   return hasConnected ? (
     <Button
