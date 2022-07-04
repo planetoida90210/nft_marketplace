ccf1dfbb1381c84aa7ab62c1e83a76cf98e64a1f
planetoida90210 <galeriakoloru90210@gmail.com>
2022-07-04 23:28:19 +0200

    testing createsale fn - connect to web3provider

--- context/NFTContext.js
+++ context/NFTContext.js
@@ -8,6 +8,8 @@ import { MarketAddress, MarketAddressABI } from './constans';
 
 const client = ipfsHttpClient('https://ipfs.infura.io:5001/api/v0');
 
+const fetchContract = (signerOrProvider) => new ethers.Contract(MarketAddress, MarketAddressABI, signerOrProvider);
+
 export const NFTContext = React.createContext();
 
 export const NFTProvider = ({ children }) => {
@@ -28,6 +30,7 @@ export const NFTProvider = ({ children }) => {
 
   useEffect(() => {
     checkIfWalletIsConnected();
+    createSale('test', '0.025');
   }, []);
 
   const connectWallet = async () => {
@@ -50,6 +53,38 @@ export const NFTProvider = ({ children }) => {
     } catch (error) {
       console.log('Error uploading file to IPFS.');
     }
+  };
+
+  const createNFT = async (formInput, fileUrl, router) => {
+    const { name, description, price } = formInput;
+
+    if (!name || !description || !price || !fileUrl) return;
+
+    const data = JSON.stringify({ name, description, imae: fileUrl });
+
+    try {
+      const added = await client.add(data);
+
+      const url = `https://ipfs.infura.io/ipfs/${added.path}`;
+
+      await createSale(url, price);
+
+      router.push('/');
+    } catch (error) {
+      console.log('Error uploading files to IPFS');
+    }
+  };
+
+  const createSale = async (url, formInputPrice, isReseling, id) => {
+    const web3Modal = new Web3Modal();
+    const connection = await web3Modal.connect();
+    const provider = new ethers.providers.Web3Provider(connection);
+    const signer = provider.getSigner();
+
+    const price = ethers.utils.parseUnits(formInputPrice, 'ether');
+    const contract = fetchContract(signer);
+
+    console.log(contract);
   };
 
   return (
