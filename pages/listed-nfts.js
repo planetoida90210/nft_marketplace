ba297ddc54b09a9a237315df8a1821880794f687
planetoida90210 <galeriakoloru90210@gmail.com>
2022-07-10 23:14:07 +0200

    create fetchmynfts fn and display nfts to sale on listed-nfts route

--- pages/listed-nfts.js
+++ pages/listed-nfts.js
@@ -4,8 +4,17 @@ import { NFTContext } from '../context/NFTContext';
 import { NFTCard, Loader } from '../components';
 
 const ListedNFTs = () => {
+  const { fetchMyNFTsOrListedNFTs } = useContext(NFTContext);
   const [nfts, setNfts] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    fetchMyNFTsOrListedNFTs('fetchItemsListed')
+      .then((items) => {
+        setNfts(items);
+        setIsLoading(false);
+      });
+  }, []);
 
   if (isLoading) {
     return (
@@ -24,8 +33,15 @@ const ListedNFTs = () => {
   }
 
   return (
-    <div>
-      Listed NFTs
+    <div className="flex justify-center sm:px-4 p-12 min-h-screen">
+      <div className="w-full minmd:w-4/5">
+        <div className="mt-4">
+          <h2 className="font-poppins dark:text-white text-nft-black-1 text-2xl font-semibold mt-2 ml-4 sm:ml-2">NFTs Listed for Sale</h2>
+          <div className="mt-3 w-full flex flex-wrap justify-start md:justify-center">
+            {nfts.map((nft) => <NFTCard key={nft.tokenId} nft={nft} />)}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
