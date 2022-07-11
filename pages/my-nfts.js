5b92fc372b68b3788071e871e287be164886ff5d
planetoida90210 <galeriakoloru90210@gmail.com>
2022-07-11 10:43:24 +0200

    display jsx of my-nfts

--- pages/my-nfts.js
+++ pages/my-nfts.js
@@ -36,7 +36,14 @@ const MyNFTs = () => {
         <div className="flexCenter sm:p-4 p-16">
           <h1 className="font-poppins dark:text-white text-nft-black-1 font-extrabold text-3xl">No NFTs Owned</h1>
         </div>
-      ) : (<div />) }
+      ) : (
+        <div className="sm:px-4 p-12 w-full minmd:w-4/5 flexCenter flex-col">
+          <div className="flex-1 w-full flex flex-row sm:flex-col px-4 xs:px-0 minlg:px-8">Searchbar</div>
+          <div className="mt-3 w-full flex flex-wrap">
+            {nfts.map((nft) => <NFTCard key={nft.tokenId} nft={nft} />)}
+          </div>
+        </div>
+      ) }
     </div>
   );
 };
