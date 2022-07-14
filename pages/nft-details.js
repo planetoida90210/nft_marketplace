520bf2c5a7849aaedac7d01e1849ec823b58b827
planetoida90210 <galeriakoloru90210@gmail.com>
2022-07-14 18:00:13 +0200

    jsx of nft details seller

--- pages/nft-details.js
+++ pages/nft-details.js
@@ -31,8 +31,22 @@ const NFTDetails = () => {
   return (
     <div className="relative flex justify-center md:flex-col min-h-screen">
       <div className="relative flex-1 flexCenter sm:px-4 p-12 border-r md:border-r-0 md:border-b dark:border-nft-black-1 border-nft-gray-1">
-        <div>
-          <Image src={nft.image} />
+        <div className="relative w-557 midmd::w-2/3 midmd:h-2/3 sm:w-full sm:h-300 h-557">
+          <Image src={nft.image} objectFit="cover" className="rounded-xl shadow-lg" layout="fill" />
+        </div>
+      </div>
+      <div className="flex-1 justify-start sm:px-4 p-12 sm:pb-4">
+        <div className="flex flex-row sm:flex-col">
+          <h2 className="font-poppins dark:text-white text-nft-black-1 font-semibold text-2xl minlg:text-3xl">{nft.name}</h2>
+        </div>
+        <div className="mt-10">
+          <p className="font-poppins dark:text-white text-nft-black-1 text-xs minlg:text-base font-normal">Creator</p>
+          <div className="flex flex-row items-center mt-3">
+            <div className="relative w-12 h-12 minlg:w-20 minlg:h-20 mr-2">
+              <Image src={images.creator1} objectFit="cover" classsName="rounded-full" />
+            </div>
+            <p className="font-poppins dark:text-white text-nft-black-1 text-xs minlg:text-base font-semibold">{shortenAddress(nft.seller)}</p>
+          </div>
         </div>
       </div>
     </div>
