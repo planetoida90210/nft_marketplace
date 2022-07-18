00a0df1058153ff9df9de9d0cd498187b10a6b47
planetoida90210 <galeriakoloru90210@gmail.com>
2022-07-18 19:32:21 +0200

    boilerplate ressel nft

--- pages/my-nfts.js
+++ pages/my-nfts.js
@@ -48,7 +48,7 @@ const MyNFTs = () => {
         <div className="sm:px-4 p-12 w-full minmd:w-4/5 flexCenter flex-col">
           <div className="flex-1 w-full flex flex-row sm:flex-col px-4 xs:px-0 minlg:px-8">Searchbar</div>
           <div className="mt-3 w-full flex flex-wrap">
-            {nfts.map((nft) => <NFTCard key={nft.tokenId} nft={nft} />)}
+            {nfts.map((nft) => <NFTCard key={nft.tokenId} nft={nft} onProfilePage />)}
           </div>
         </div>
       ) }
--- pages/resell-nft.js
+++ pages/resell-nft.js
@@ -1,7 +1,44 @@
-import React from 'react';
+import { useState, useEffect, useContext } from 'react';
+import { useRouter } from 'next/router';
+import axios from 'axios';
 
-const ResellNFT = () => (
-  <div>ResellNFT</div>
-);
+import { NFTContext } from '../context/NFTContext';
+import { Loader, Button, Input } from '../components';
 
+const ResellNFT = () => {
+  const {} = useContext(NFTContext);
+  const router = useRouter();
+  const { tokenId, tokenURI } = router.query;
+  const [price, setPrice] = useState('');
+  const [image, setImage] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
+
+  const fetchNFT = async () => {
+    const { data } = await axios.get(tokenURI);
+
+    setPrice(data.price);
+    setImage(data.image);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    if (tokenURI) fetchNFT();
+  }, [tokenURI]);
+
+  if (isLoading) {
+    return (
+      <div className="flex-start min-h-screen">
+        <Loader />
+      </div>
+    );
+  }
+
+  return (
+    <div className="flex justify-center sm:px-4 p-12">
+      <div className="w-3/5 md:w-full">
+        <h1 className="font-poppins dark:text-white text-nft-black-1 font-semibold text-2xl">Resell NFT</h1>
+      </div>
+    </div>
+  );
+};
 export default ResellNFT;
