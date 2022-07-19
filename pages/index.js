70dc11071eec3954b78b31837a4466b353606282
planetoida90210 <galeriakoloru90210@gmail.com>
2022-07-19 13:49:50 +0200

    create and display top creators fn to sort by the eth collections

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,8 @@ import { Banner, CreatorCard, NFTCard } from '../components';
 import images from '../assets/assets';
 import { makeId } from '../utils/makeId';
 import { NFTContext } from '../context/NFTContext';
+import { getCreators } from '../utils/getTopCreators';
+import { shortenAddress } from '../utils/shortenAddress';
 
 const Home = () => {
   const [hideButtons, setHideButtons] = useState(false);
@@ -53,6 +55,8 @@ const Home = () => {
     };
   });
 
+  const topCreators = getCreators(nfts);
+
   return (
     <div className="flex justify-center sm:px-4 p-12">
       <div className="w-full minmd:w-4/5">
@@ -66,9 +70,12 @@ const Home = () => {
           <h1 className="font-poppins dark:text-white text-nft-black-1 text-2xl minlg:text-4xl font-semibold ml-4 xs:ml-0">Best Creators</h1>
           <div className="relative flex-1 max-w-full flex mt-3" ref={parentRef}>
             <div className="flex flex-row w-max overflow-x-scroll no-scrollbar select-none" ref={scrollRef}>
-              {[6, 7, 8, 9, 10].map((i) => (
-                <CreatorCard key={`creator-${i}`} rank={i} creatorImage={images[`creator${i}`]} creatorName={`0x${makeId(3)}...${makeId(4)}`} creatorEths={10 - i * 0.5} />
+              {topCreators.map((creator, i) => (
+                <CreatorCard key={creator.seller} rank={i + 1} creatorImage={images[`creator${i + 1}`]} creatorName={shortenAddress(creator.seller)} creatorEths={creator.sum} />
               ))}
+              {/* {[6, 7, 8, 9, 10].map((i) => (
+                <CreatorCard key={`creator-${i}`} rank={i} creatorImage={images[`creator${i}`]} creatorName={`0x${makeId(3)}...${makeId(4)}`} creatorEths={10 - i * 0.5} />
+              ))} */}
               {!hideButtons && (
               <>
                 <div onClick={() => handleScroll('left')} className="absolute w-8 h-8 minlg:w-12 minlg:h-12 top-45 cursor-pointer left-0">
