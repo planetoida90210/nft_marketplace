a9c267ffbddd72dac5fbf90704324b0165b7696a
planetoida90210 <galeriakoloru90210@gmail.com>
2022-07-27 22:53:21 +0200

    fix css mobile bugs

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -9,7 +9,7 @@ import images from '../assets/assets';
 import { Button } from '.';
 import { NFTContext } from '../context/NFTContext';
 
-const MenuItems = ({ isMobile, active, setActive }) => {
+const MenuItems = ({ isMobile, active, setActive, setIsOpen }) => {
   const generateLink = (i) => {
     switch (i) {
       case 0: return '/';
@@ -22,7 +22,14 @@ const MenuItems = ({ isMobile, active, setActive }) => {
   return (
     <ul className={`list-none flexCenter flex-row ${isMobile ? 'flex-col h-full' : ''}`}>
       {['Explore NFTs', 'Listed NFTs', 'My NFTs'].map((item, i) => (
-        <li key={i} onClick={() => { setActive(item); }} className={`flex flex-row items-center font-poppins font-semibold text-base dark:hover:text-white hover:text-nft-dark mx-3 ${active === item ? 'dark:text-white text-nft-black-1' : 'dark:text-nft-gray-3 text-nft-gray-2'}`}>
+        <li
+          key={i}
+          onClick={() => {
+            setActive(item);
+            if (isMobile) setIsOpen(false);
+          }}
+          className={`flex flex-row items-center font-poppins font-semibold text-base dark:hover:text-white hover:text-nft-dark mx-3 ${active === item ? 'dark:text-white text-nft-black-1' : 'dark:text-nft-gray-3 text-nft-gray-2'}`}
+        >
           <Link href={generateLink(i)}>{item}</Link>
         </li>
       ))}
@@ -30,7 +37,7 @@ const MenuItems = ({ isMobile, active, setActive }) => {
   );
 };
 
-const ButtonGroup = ({ setActive, router }) => {
+const ButtonGroup = ({ setActive, router, setIsOpen }) => {
   const { connectWallet, currentAccount } = useContext(NFTContext);
 
   return currentAccount ? (
@@ -39,7 +46,7 @@ const ButtonGroup = ({ setActive, router }) => {
       classStyles="mx-2 rounded-xl"
       handleClick={() => {
         setActive('');
-
+        setIsOpen(false);
         router.push('/create-nft');
       }}
     />
@@ -83,13 +90,24 @@ const Navbar = () => {
     <nav className="flexBetween w-full fixed z-10 p-4 flex-row border-b dark:bg-nft-dark bg-white dark:border-nft-black-1 border-nft-gray-1">
       <div className="flex flex-1 flex-row justify-start">
         <Link href="/">
-          <div className="flexCenter md:hidden cursor-pointer" onClick={() => {}}>
+          <div
+            className="flexCenter md:hidden cursor-pointer"
+            onClick={() => {
+              setActive('Explore NFTs');
+            }}
+          >
             <Image src={images.logo02} objectFit="contain" width={32} height={32} alt="logo" />
             <p className="dark:text-white text-nft-black-1 font-semibold text-lg ml-1">CryptoKet</p>
           </div>
         </Link>
         <Link href="/">
-          <div className="hidden md:flex" onClick={() => {}}>
+          <div
+            className="hidden md:flex"
+            onClick={() => {
+              setActive('Explore NFTs');
+              setIsOpen(false);
+            }}
+          >
             <Image src={images.logo02} objectFit="contain" width={32} height={32} alt="logo" />
           </div>
         </Link>
@@ -127,10 +145,10 @@ const Navbar = () => {
           {isOpen && (
             <div className="fixed inset-0 top-65 dark:bg-nft-dark bg-white z-10 nav-h flex justify-between flex-col">
               <div className="flex-1 p-4">
-                <MenuItems active={active} setActive={setActive} isMobile />
+                <MenuItems active={active} setActive={setActive} isMobile setIsOpen={setIsOpen} />
               </div>
               <div className="p-4 border-t dark:border-nft-black-1 border-nft-gray-1">
-                <ButtonGroup setActive={setActive} router={router} />
+                <ButtonGroup setActive={setActive} router={router} setIsOpen={setIsOpen} />
               </div>
             </div>
           )}
--- components/SearchBar.jsx
+++ components/SearchBar.jsx
@@ -38,7 +38,7 @@ const SearchBar = ({ activeSelect, setActiveSelect, clearSearch, handleSearch })
           value={debouncedSearch}
         />
       </div>
-      <div onClick={() => setToggle((prevToggle) => !prevToggle)} className="relative flexBetween ml-4 sm:ml-0 sm:mt-2 min-w-190 cursor-pointer dark:bg-nft-black-2 bg-white border dark:border-nft-black-2 border-nft-gray-2 px-4 rounded-md"> <p className="font-poppins dark:text-white text-nft-black-1 font-normal text-xs">Recently Listed</p>
+      <div onClick={() => setToggle((prevToggle) => !prevToggle)} className="relative flexBetween ml-4 sm:ml-0 sm:mt-2 min-w-190 cursor-pointer dark:bg-nft-black-2 bg-white border dark:border-nft-black-2 border-nft-gray-2 py-3 px-4 rounded-md"> <p className="font-poppins dark:text-white text-nft-black-1 font-normal text-xs">Recently Listed</p>
         <Image src={images.arrow} objectFit="contain" width={15} height={15} alt="arrow" className={theme === 'light' && 'filter invert'} />
         {toggle && (
         <div className="absolute top-full left-0 right-0 w-full mt-3 z-10 dark:bg-nft-black-2 bg-white border dark:border-nft-black-2 border-nft-gray-2 py-3 px-4 rounded-md">
